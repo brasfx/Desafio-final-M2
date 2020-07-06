ef4251d28011e4729fc28040205fdfde2150e522
davi.ribeiro <foxpc18@gmail.com>
2020-07-06 18:33:27 -0300

    Adicionando dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { promises } from 'fs';
 import gradesRouter from './routes/grades.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const readFile = promises.readFile;
@@ -12,10 +15,10 @@ app.use(express.json());
 app.use('/grades', gradesRouter);
 
 /*Iniciandi o servidor local*/
-app.listen(3000, async () => {
+app.listen(process.env.PORT, async () => {
   try {
     await readFile(fileName, 'utf8');
-    console.log(`Servidor rodando na porta 3000`);
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
   } catch (err) {
     console.log('Ocorreu um erro na aplicação!', err);
   }
